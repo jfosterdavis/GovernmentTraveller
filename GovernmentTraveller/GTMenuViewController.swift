d0b24a61b97edb65141ec73b6bdb41171fc3ae95
jfosterdavis <j.foster.davis@gmail.com>
2018-07-14 19:34:54 +0300

    Skeleton set up with menu and ZMAppFoundation.

---
+++ GovernmentTraveller/GTMenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GTMenuViewController.swift
+//  GovernmentTraveller
+//
+//  Created by Jacob Foster Davis on 14Jul18.
+//  Copyright © 2018 Zero Mu, LLC. All rights reserved.
+//
+
+import Foundation
+import ZMAppFoundation
+
+class GTMenuViewController: ZMMenuTableViewController {
+    
+    override open func setZMAllMenuItems() {
+        //default function is to take the standard items coded into ZMMenuItems.StandardItems
+        
+        var allZMMenuItems = [ZMMenuItem]()
+        
+        //allZMMenuItems.append(contentsOf: ExampleMenuItems.StandardItems)
+        allZMMenuItems.append(contentsOf: ZMMenuItems.StandardItems)
+        
+        zmAllMenuItems = allZMMenuItems
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '10.0'
+
+# dependency overrides
+def dependency_overrides
+    pod 'GaugeKit', git: 'https://github.com/skywinder/GaugeKit', branch: 'swift-4'
+end
+
+target 'GovernmentTraveller' do
+
+dependency_overrides
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+
+
+  # Pods for GovernmentTraveller
+  pod 'ZMAppFoundation'
+
+  target 'GovernmentTravellerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
