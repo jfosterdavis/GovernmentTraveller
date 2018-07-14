6186456de0a3051e7d6e80e5541d639cbe517de3
jfosterdavis <j.foster.davis@gmail.com>
2018-07-14 18:58:21 +0300

    Initial Commit

---
+++ GovernmentTraveller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GovernmentTraveller
+//
+//  Created by Jacob Foster Davis on 14Jul18.
+//  Copyright © 2018 Zero Mu, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
