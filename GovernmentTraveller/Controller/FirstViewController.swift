b5c0af756b693d40f156ca64cb241582528159ae
jfosterdavis <j.foster.davis@gmail.com>
2018-07-20 15:34:24 -0500

    May have just been able to add a Trip entity to CoreData.

--- GovernmentTraveller/Controller/FirstViewController.swift
+++ GovernmentTraveller/Controller/FirstViewController.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  FirstViewController.swift
 //  GovernmentTraveller
 //
@@ -8,16 +8,35 @@
 
 import UIKit
 import ZMAppFoundation
+import CoreData
 
 
-class FirstViewController: UIViewController {
+class FirstViewController: CoreDataViewController {
 
+    //CoreData FRC Keys
+    let keyTrips = "keyTrips"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         //add custom menu items to the ZMMenu
         ZMMenuItems.CustomItems.append(contentsOf: CustomMenuItems.Items)
+        
+        //setup CoreData
+        _ = setupFetchedResultsController(frcKey: keyTrips, entityName: "Trip", sortDescriptors: [],  predicate: nil)
+        
+        //add a trip
+        guard let fc = frcDict[keyTrips] else {
+            return
+        }
+           
+        let newTrip = Trip(entity: NSEntityDescription.entity(forEntityName: "Trip", in: stack.context)!, insertInto: fc.managedObjectContext)
+        newTrip.firstDay = Date()
+        newTrip.lastDay = Date.distantFuture
+        newTrip.name = "A new Trip"
+        stack.save()
+        
     }
 
     override func didReceiveMemoryWarning() {
